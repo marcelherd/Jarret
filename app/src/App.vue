56fe6dfb537e47e63ad47c704b9bb5dff3eec32e
Marcel Herd <me@marcelherd.com>
2021-06-06 18:14:13 +0200

    Boilerplate vue app

---
+++ app/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <input v-model="message" @keyup.enter="submit"/>
+  <ul>
+    <li v-for="(msg, index) in messages" :key="index">
+      {{msg}}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  },
+  data() {
+    return {
+      message: '',
+      messages: []
+    }
+  },
+  methods: {
+    submit() {
+      this.messages.push(this.message);
+      this.message = '';
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
